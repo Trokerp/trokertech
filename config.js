93838aaebc422490cbe62c40c772e389d760a61b
Trokerp <116848738+Trokerp@users.noreply.github.com>
2023-07-01 10:16:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'QUEEN GRACE'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'TROKER-TECH' 
+global.author = '@TROKERPOFFICIAL' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
